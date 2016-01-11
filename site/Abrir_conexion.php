f9b03e8fc090323413b574ff004a38f8965c13e9
kuncho <sergiogaguero@gmail.com>
2016-01-11 17:46:44 -0300

    se sacan comentarios

--- site/Abrir_conexion.php
+++ site/Abrir_conexion.php
@@ -1,7 +1,7 @@
 <?php
-	echo "hola putos";
+
 	$dbhost="127.0.0.1";  // host del MySQL (generalmente localhost)
-	$dbusuario="root"; // aqui debes ingresar el nombre de usuario
+	$dbusuario="kun"; // aqui debes ingresar el nombre de usuario
                       // para acceder a la base
 	$dbpassword="admin1234"; // password de acceso para el usuario de la
                       // linea anterior
--- site/Peleador.php
+++ site/Peleador.php
@@ -13,6 +13,10 @@
 		<script src="/libraries/RGraph.common.dynamic.js" ></script>
 		<script src="/libraries/RGraph.common.tooltips.js" ></script>
 		<script src="/libraries/RGraph.common.effects.js" ></script>
+         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
 		
 	</head>
 	<body id="page3">
@@ -80,10 +84,10 @@
 							
 						
 							if (file_exists("images/Peleadores/$id.jpg")){
-								$img = "/images/Peleadores/$id.jpg";
+								$img = "images/Peleadores/$id.jpg";
 							}else
 							{
-								$img = "/images/Peleadores/Gen1.jpg";
+								$img = "images/Peleadores/Gen1.jpg";
 							}
 							
 							echo "
